7125816843eaeb3fd406312b300bd7a437cddb4c
varaprasad konjeti <varaprasad.konjeti@gmail.com>
2019-08-02 15:19:49 -0700

    updated code for extracting weather data

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 // specific env vars in the future
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 // currently anyone can access our API
 app.use(cors());
@@ -18,7 +18,23 @@ app.get('/location', (request, response) => {
   // get location data from geo.json file
   const locationData = searchToLatLong(request.query.data);
   response.send(locationData);
+    
 });
+
+app.get('/weather',(request, response) => {
+  //get weather data from darksky.json file
+    const weatherData = searchForWeather(request.query.data);
+    response.send(weatherData);
+});
+
+function searchForWeather(query) {
+  const darkskyData = require('./data/darksky.json');
+  var wdata=[];
+  darkskyData.daily.data.forEach(element => {
+    wdata.push(new Weather(element));
+  });
+  return wdata;
+}
 
 function searchToLatLong(query) {
   const geoData = require('./data/geo.json');
@@ -26,12 +42,18 @@ function searchToLatLong(query) {
   location.search_query = query;
   return location;
 }
-
 function Location(data) {
+  //this.search_query = data.search_query;
   this.formatted_query = data.formatted_address;
   this.latitude = data.geometry.location.lat;
   this.longitude = data.geometry.location.lng;
 }
+
+function Weather(data) {
+  this.forecast = data.summary;
+  this.time = data.time;
+}
+
 
 app.listen(PORT, () => {
   console.log ('This is lab06');
